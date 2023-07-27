0c7a9d787149ae2ad6e4cef25949fb43ddba816e
Michał <m.szleszkowski@gmail.com>
2023-07-27 09:57:16 +0200

    Add Builder

--- Clean Swift/Module.xctemplate/___FILEBASENAME_Builder.swift
+++ Clean Swift/Module.xctemplate/___FILEBASENAME_Builder.swift
@@ -8,8 +8,14 @@ final class ___VARIABLE_productName:identifier___ViewController: UIViewControlle
     // MARK: - Public Properties
     var interactor: ___VARIABLE_productName:identifier___InteractorLogic?
     var router: ___VARIABLE_productName:identifier___RouterProtocol?
+    
+    // MARK: - Private Properties
+    private let ___VARIABLE_productName:identifier___View = ___VARIABLE_productName:identifier___View()
+    
+    override func loadView() {
+        view = ___VARIABLE_productName:identifier___View
+    }
 }
 
 extension ___VARIABLE_productName:identifier___ViewController: ___VARIABLE_productName:identifier___ViewControllerLogic {
-    
 }
