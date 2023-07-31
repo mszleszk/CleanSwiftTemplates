01b4338dd947e53209f4d8560b32847ae102cd58
Michał <m.szleszkowski@gmail.com>
2023-07-31 09:13:44 +0200

    Add worker

---
+++ Clean Swift/Worker.xctemplate/___FILEBASENAME___Worker.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+protocol ___VARIABLE_productName:identifier___WorkerProtocol {
+    
+}
+
+final class ___VARIABLE_productName:identifier___Worker {
+    
+}
+
+extension ___VARIABLE_productName:identifier___Worker: ___VARIABLE_productName:identifier___WorkerProtocol {
+    
+}
